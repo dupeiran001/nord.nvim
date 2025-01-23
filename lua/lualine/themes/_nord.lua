2f93091b929bb2dc3537bea8032ab1d2778ebfc8
Peiran Du <dupeiran3@gmail.com>
2025-01-22 22:42:26 -0600

    fix(lualine): lualine_b highlight

--- lua/lualine/themes/_nord.lua
+++ lua/lualine/themes/_nord.lua
@@ -10,33 +10,33 @@ function M.get(style)
 
   hl.normal = {
     a = { bg = colors.blue, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.blue },
+    b = { bg = colors.nord1_gui, fg = colors.blue },
     c = { bg = colors.bg_statusline, fg = colors.fg_sidebar },
   }
 
   hl.insert = {
     a = { bg = colors.green, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.green },
+    b = { bg = colors.nord1_gui, fg = colors.green },
   }
 
   hl.command = {
     a = { bg = colors.yellow, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.yellow },
+    b = { bg = colors.nord1_gui, fg = colors.yellow },
   }
 
   hl.visual = {
     a = { bg = colors.magenta, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.magenta },
+    b = { bg = colors.nord1_gui, fg = colors.magenta },
   }
 
   hl.replace = {
     a = { bg = colors.red, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.red },
+    b = { bg = colors.nord1_gui, fg = colors.red },
   }
 
   hl.terminal = {
     a = { bg = colors.green1, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.green1 },
+    b = { bg = colors.nord1_gui, fg = colors.green1 },
   }
 
   hl.inactive = {
