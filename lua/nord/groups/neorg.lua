908cce900143ea2a0f7b21d426cf7c2824d72b6e
Peiran Du <dupeiran3@gmail.com>
2025-01-30 16:31:20 -0600

    feat(neorg): add highlight for quotes

--- lua/nord/groups/neorg.lua
+++ lua/nord/groups/neorg.lua
@@ -20,6 +20,19 @@ function M.get(c)
     ["@neorg.headings.5.prefix"]       = { link = "markdownH5" },
     ["@neorg.headings.6.title"]        = { link = "markdownH6" },
     ["@neorg.headings.6.prefix"]       = { link = "markdownH6" },
+
+    ["@neorg.quotes.1.prefix.norg"]   = { link = "markdownH1" },
+    ["@neorg.quotes.1.content.norg"]   = { link = "markdownH1" },
+    ["@neorg.quotes.2.prefix.norg"]   = { link = "markdownH2" },
+    ["@neorg.quotes.2.content.norg"]   = { link = "markdownH2" },
+    ["@neorg.quotes.3.prefix.norg"]   = { link = "markdownH3" },
+    ["@neorg.quotes.3.content.norg"]   = { link = "markdownH3" },
+    ["@neorg.quotes.4.prefix.norg"]   = { link = "markdownH4" },
+    ["@neorg.quotes.4.content.norg"]   = { link = "markdownH4" },
+    ["@neorg.quotes.5.prefix.norg"]   = { link = "markdownH5" },
+    ["@neorg.quotes.5.content.norg"]   = { link = "markdownH5" },
+    ["@neorg.quotes.6.prefix.norg"]   = { link = "markdownH6" },
+    ["@neorg.quotes.6.content.norg"]   = { link = "markdownH6" },
   }
 end
 
