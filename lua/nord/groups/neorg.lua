6cc8b92e5187eadc4ddbb6b293709c3fe2017db4
Peiran Du <dupeiran3@gmail.com>
2025-04-25 17:03:57 -0500

    fix(neorg): fix neorg color group name changes

--- lua/nord/groups/neorg.lua
+++ lua/nord/groups/neorg.lua
@@ -9,17 +9,29 @@ function M.get(c)
   -- stylua: ignore
   return {
     ["@neorg.headings.1.title"]        = { link = "markdownH1" },
-    ["@neorg.headings.1.prefix"]       = { link = "markdownH1" },
+    ["@neorg.headings.1.title.norg"]        = { link = "markdownH1" },
+    ["@neorg.headings.1.prefix.norg"]       = { link = "markdownH1" },
+
     ["@neorg.headings.2.title"]        = { link = "markdownH2" },
-    ["@neorg.headings.2.prefix"]       = { link = "markdownH2" },
+    ["@neorg.headings.2.title.norg"]        = { link = "markdownH2" },
+    ["@neorg.headings.2.prefix.norg"]       = { link = "markdownH2" },
+
     ["@neorg.headings.3.title"]        = { link = "markdownH3" },
-    ["@neorg.headings.3.prefix"]       = { link = "markdownH3" },
+    ["@neorg.headings.3.title.norg"]        = { link = "markdownH3" },
+    ["@neorg.headings.3.prefix.norg"]       = { link = "markdownH3" },
+
     ["@neorg.headings.4.title"]        = { link = "markdownH4" },
-    ["@neorg.headings.4.prefix"]       = { link = "markdownH4" },
+    ["@neorg.headings.4.title.norg"]        = { link = "markdownH4" },
+    ["@neorg.headings.4.prefix.norg"]       = { link = "markdownH4" },
+
     ["@neorg.headings.5.title"]        = { link = "markdownH5" },
-    ["@neorg.headings.5.prefix"]       = { link = "markdownH5" },
+    ["@neorg.headings.5.title.norg"]        = { link = "markdownH5" },
+    ["@neorg.headings.5.prefix.norg"]       = { link = "markdownH5" },
+
     ["@neorg.headings.6.title"]        = { link = "markdownH6" },
-    ["@neorg.headings.6.prefix"]       = { link = "markdownH6" },
+    ["@neorg.headings.6.title.norg"]        = { link = "markdownH6" },
+    ["@neorg.headings.6.prefix.norg"]       = { link = "markdownH6" },
+
 
     ["@neorg.quotes.1.prefix.norg"]   = { link = "markdownH1" },
     ["@neorg.quotes.1.content.norg"]   = { link = "markdownH1" },
